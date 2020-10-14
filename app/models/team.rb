e66aa538ba2143d6a2d29c50096f2923356b722d
Matthew Kirby <matt.kirby@yahoo.com>
2020-10-14 14:37:22 -0400

    added team associations and validations

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,11 @@
 class Team < ApplicationRecord
+    has_many :trivia_squads
+    has_many :players, through: :trivia_squads
+    validates :name, :max_member_count, :slogan, :winning_note, :losing_note, presence: true
+    validates :name, :slogan, :winning_note, :losing_note, uniqueness: true
+    validates :slogan, length: {minimum: 2, maximum: 40}
+    validates :winning_note, :losing_note, length: {minimum: 30, maximum: 70}
+    validates :max_member_count, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 6}
+    validates :recruiting, inclusion: { in: [true, false] }
+    validates :recruiting, exclusion: { in: [nil] }
 end
