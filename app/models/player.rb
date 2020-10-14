930a76dda2dd51e3c22ae94b8811d643364b68d6
Matthew Kirby <matt.kirby@yahoo.com>
2020-10-14 14:27:13 -0400

    added player associations and validations

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,7 @@
 class Player < ApplicationRecord
+    has_many :trivia_squads
+    has_many :teams, through: :trivia_squads
+    validates :name, :nickname, :expertise, :catchphrase, presence: true
+    validates :nickname, :catchphrase, uniqueness: true
+    validates :catchphrase, length: {minimum: 2, maximum: 40}
 end
