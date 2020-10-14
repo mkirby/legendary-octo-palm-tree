0292ee7f3671192bc28c24e120b01a356d5eecbc
Matthew Kirby <matt.kirby@yahoo.com>
2020-10-14 14:39:11 -0400

    added TriviaSquad validations

--- app/models/trivia_squad.rb
+++ app/models/trivia_squad.rb
@@ -1,4 +1,5 @@
 class TriviaSquad < ApplicationRecord
-  belongs_to :team
-  belongs_to :player
+    belongs_to :team
+    belongs_to :player
+    validates :team_id, :player_id, presence: true
 end
